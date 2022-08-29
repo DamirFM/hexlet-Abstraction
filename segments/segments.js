8bf28da98d292162be364ac1761a422c022becc2
Damir Yakupov <yakupovdamir93@gmail.com>
2022-08-29 19:56:05 -0400

    add segment

--- segments/segments.js
+++ segments/segments.js
@@ -1,17 +1,18 @@
 import  {makeDecartPoint, getX, getY}  from 'points.js';
 
 
-function makeSegment (point1, point2) {
+function makeSegment(point1, point2) {
     const beginPoint = makeDecartPoint(getX(point1), getY(point1));
     const endPoint = makeDecartPoint(getX(point2), getY(point2));
     const segment = ({beginPoint, endPoint});
     return segment;
-  };
+};
+function getMidpointOfSegment (segment) {
   
-  
-  function getMidpointOfSegment () {
-  
-  };
+  const midPoint = ({ x: (segment.beginPoint.x + segment.endPoint.x) / 2, y: (segment.beginPoint.y + segment.endPoint.y) / 2 }) ;
+  console.log(midPoint)
+  return midPoint;
+};
   
   
   function getBeginPoint () {
