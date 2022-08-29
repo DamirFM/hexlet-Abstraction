bad5944ae0d401e11b47f38417f126a833f3ce6c
Damir Yakupov <yakupovdamir93@gmail.com>
2022-08-28 21:46:35 -0400

    add segments.js

---
+++ segments/segments.js
@@ -0,0 +1,20 @@
+function makeSegment (point1, point12) {
+    console.log(point1, point12)
+  };
+  
+  
+  function getMidpointOfSegment () {
+  
+  };
+  
+  
+  function getBeginPoint () {
+  
+  };
+  
+  
+  function getEndPoint () {
+  
+  };
+  
+  export { makeSegment, getMidpointOfSegment, getBeginPoint, getEndPoint };
