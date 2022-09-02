47bcc10aee73a714834e73e681929e8980852ff3
Damir Yakupov <yakupovdamir93@gmail.com>
2022-09-02 09:52:49 -0400

    add rect

--- rectangle/rectangle.js
+++ rectangle/rectangle.js
@@ -1,5 +1,26 @@
 import { makeDecartPoint, getX, getY, getQuadrant } from './points.js';
 
 // BEGIN (write your solution here)
+// construct
+export const makeRectangle = (point, width, height) => {
 
+};
+
+
+//selections
+const getStartPoint = (rectangle) => {
+
+}
+
+const getWidth = (rectangle) => {
+
+}
+
+const getHeight = (rectangle) => {
+
+}
+
+export const containsOrigin = (rectangle) => {
+
+}
 // END
