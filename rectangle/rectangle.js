beab4beaae2d8e367630decce4b55c66e9b7e2e4
Damir Yakupov <yakupovdamir93@gmail.com>
2022-09-03 22:50:10 -0400

    add rectangle

--- rectangle/rectangle.js
+++ rectangle/rectangle.js
@@ -1,26 +1,26 @@
 import { makeDecartPoint, getX, getY, getQuadrant } from './points.js';
 
 // BEGIN (write your solution here)
-// construct
 export const makeRectangle = (point, width, height) => {
-
-};
-
-
-//selections
-const getStartPoint = (rectangle) => {
-
-}
-
-const getWidth = (rectangle) => {
-
-}
-
-const getHeight = (rectangle) => {
-
-}
-
-export const containsOrigin = (rectangle) => {
-
-}
+    const p = makeDecartPoint(getX(point), getY(point));
+    const rectangle = makeRectangle(p, width, height);
+    console.log(rectangle)
+    return rectangle; 
+  };
+  //const rectangle = makeRectangle(makeDecartPoint, width, height)
+  
+  //selections
+  const getStartPoint = (rectangle) => makeDecartPoint(getX(point), getY(point));
+  
+  const getWidth = (rectangle) => {
+  
+  }
+  
+  const getHeight = (rectangle) => {
+  
+  }
+  
+  export const containsOrigin = (rectangle) => {
+  
+  } 
 // END
