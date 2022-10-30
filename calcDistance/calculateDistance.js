896b75cda17415955d4ae0b9acdb5a54cfbcc849
Damir Yakupov <yakupovdamir93@gmail.com>
2022-10-29 22:41:46 -0400

    add calk

--- calcDistance/calculateDistance.js
+++ calcDistance/calculateDistance.js
@@ -4,6 +4,7 @@
 //point1 = [0, 0];
 //point2 = [3, 4];
 //calculateDistance(point1, point2); // 5
+
 export default function calculateDistance(point1, point2) {
     const p1 = (point1[0] - point2[0]) ** 2;
     const p2 = (point1[1] - point2[1]) ** 2;
